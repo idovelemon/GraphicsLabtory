54ab975d8d0463b9c06c53c72237c9bd63463531
idovelemon <1322600812@qq.com>
2018-03-14 21:32:18 +0800

    Add PBR Texture demo

---
+++ glbcodebase/graphicslab/glb_pbs_texture/glb_pbs_texture.h
@@ -0,0 +1,21 @@
+//----------------------------------------------------------------------
+// Declaration: Copyright (c), by i_dovelemon, 2018. All right reserved.
+// Author: i_dovelemon[1322600812@qq.com]
+// Date: 2018 / 03 / 14
+// Brief: Demostrate how to use PBR Texture
+//----------------------------------------------------------------------
+#ifndef GLB_GLB_PBS_TEXTURE_H_
+#define GLB_GLB_PBS_TEXTURE_H_
+
+#include <stdio.h>
+
+// Other Support library Header
+#include <GL\glew.h>
+#include <GL\GL.h>
+
+// Host Project Header
+#include "glb.h"
+
+using namespace glb;
+
+#endif  // GLB_GLB_PBS_TEXTURE_H_
