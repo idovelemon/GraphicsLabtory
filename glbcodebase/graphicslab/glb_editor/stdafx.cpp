31cea3aafcf8a3621c3a2a47d3854415e418d86a
idovelemon <1322600812@qq.com>
2018-05-31 20:16:33 +0800

    Remove material editor project

--- glbcodebase/graphicslab/glb_editor/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// glb_editor.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
