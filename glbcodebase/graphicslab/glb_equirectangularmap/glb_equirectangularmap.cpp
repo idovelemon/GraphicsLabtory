9bfe4ee7ac0b4eb322536c834d19fe74d1632171
idovelemon <1322600812@qq.com>
2018-10-11 07:39:45 +0800

    Setup decal map size

--- glbcodebase/graphicslab/glb_equirectangularmap/glb_equirectangularmap.cpp
+++ glbcodebase/graphicslab/glb_equirectangularmap/glb_equirectangularmap.cpp
@@ -143,6 +143,8 @@ int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR cmdLine,
     config.screen_height = 600;
     config.shadow_map_width = 1024;
     config.shadow_map_height = 1024;
+    config.decalMapWidth = 1024;
+    config.decalMapHeight = 1024;
     config.icon = IDI_ICON1;
     if (!glb::app::Application::Initialize(ApplicationERMap::Create, hInstance, config)) {
         return 0;
